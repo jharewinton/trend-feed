cb2146c1961bc234092b424224821d375ef8848e
jharewinton <jonathan.hare-winton@guardian.co.uk>
2016-06-09 15:20:41 +0100

    start parsing govt petition data

--- app/controllers/GvtPetitionsFeed.scala
+++ app/controllers/GvtPetitionsFeed.scala
@@ -1,16 +1,32 @@
 package controllers
 
-import play.api.libs.json.{JsValue, Json}
-import services.HTTP
+import model.Petition
+import play.api.libs.json._
 
 import scala.io.Source
 
-object GvtPetitionsFeed extends HTTP {
-
-  lazy val json = getFeedJson()
+object GvtPetitionsFeed {
 
   def getFeedJson(): JsValue = {
     Json.parse(Source.fromURL("https://petition.parliament.uk/petitions.json?state=open").mkString)
   }
+
+  def createPetition(jsValue: JsValue): Petition = {
+    val attributes = jsValue \ "attributes"
+    val name = (attributes \ "action").get.as[String]
+    val sigCount = (attributes \ "signature_count").get.as[Int]
+    val description = (attributes \ "background").get.as[String]
+    val url = (jsValue \ "links" \ "self").get.as[String]
+    Petition(name, sigCount, description, url)
+  }
+
+  def getPetitions():Seq[Petition] = {
+    getFeedJson() \ "data" match {
+      case JsDefined(JsArray(array)) => array.map(createPetition)
+      case _ => throw new Exception
+    }
+  }
+
+
 
 }
--- build.sbt
+++ build.sbt
@@ -10,8 +10,7 @@ libraryDependencies ++= Seq(
   jdbc,
   cache,
   ws,
-  "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test,
-  "org.apache.httpcomponents" % "httpclient" % "4.3.4"
+  "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test
 )
 
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
